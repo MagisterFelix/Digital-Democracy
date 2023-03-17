84abaee2e1a78d209a7b0821faa86415c1ad9766
MagisterFelix <felix.vlad.14@gmail.com>
2023-03-17 13:23:10 +0200

    Refactor code

--- blockchain/voting/contracts/Voting.sol
+++ blockchain/voting/contracts/Voting.sol
@@ -3,37 +3,58 @@
 pragma solidity ^0.8.0;
 
 contract Voting {
-    struct Bill {
-        uint256 id;
+    struct Ballot {
         string title;
         string[] options;
         uint256 endTime;
     }
 
-    uint256 private nextBillId;
-    mapping(uint256 => Bill) public bills;
-    mapping(uint256 => mapping(string => bool)) public hasVoted;
-    mapping(uint256 => mapping(uint256 => uint256)) public voteCounts;
+    mapping(bytes32 => Ballot) private ballots;
+    mapping(bytes32 => mapping(string => bool)) private hasVoted;
+    mapping(bytes32 => mapping(uint8 => uint256)) private voteCounts;
 
-    function createBill(string memory _title, string[] memory _options, uint256 _duration) public {
-        uint256 newBillId = nextBillId++;
+    modifier ballotExists(bytes32 _ballotAddress) {
+        require(bytes(ballots[_ballotAddress].title).length > 0, "Ballot not found.");
+        _;
+    }
+
+    event BallotCreated(bytes32 ballotAddress, string title, string[] options, uint256 endTime);
+    event VoteCreated(bytes32 ballotAddress, string passport, uint8 option);
+
+    function createBallot(string memory _title, string[] memory _options, uint256 _duration) external returns (bytes32) {
+        bytes32 id = keccak256(abi.encodePacked(_title, block.timestamp));
         uint256 endTime = block.timestamp + _duration;
-        bills[newBillId] = Bill(newBillId, _title, _options, endTime);
+        ballots[id] = Ballot(_title, _options, endTime);
+        emit BallotCreated(id, _title, _options, endTime);
+        return id;
     }
 
-    function vote(uint256 _billId, uint256 _optionIndex, string memory _passportHash) public {
-        require(bills[_billId].id != 0, "Bill not found.");
-        require(block.timestamp <= bills[_billId].endTime, "Voting has ended.");
-        require(_optionIndex < bills[_billId].options.length, "Invalid option index.");
-        require(!hasVoted[_billId][_passportHash], "User has already voted.");
-
-        hasVoted[_billId][_passportHash] = true;
-        voteCounts[_billId][_optionIndex]++;
+    function getBallot(bytes32 _ballotAddress) ballotExists(_ballotAddress) external view returns (Ballot memory) {
+        return ballots[_ballotAddress];
     }
 
-    function getBillOptions(uint256 _billId) public view returns (string[] memory) {
-        require(bills[_billId].id != 0, "Bill not found.");
-        return bills[_billId].options;
+    function hasEnded(bytes32 _ballotAddress) ballotExists(_ballotAddress) public view returns (bool) {
+        return ballots[_ballotAddress].endTime < block.timestamp;
+    }
+
+    function hasVote(bytes32 _ballotAddress, string memory _passport) ballotExists(_ballotAddress) external view returns (bool) {
+        return hasVoted[_ballotAddress][_passport];
+    }
+
+    function getVoteCounts(bytes32 _ballotAddress) ballotExists(_ballotAddress) external view returns (uint256[] memory) {
+        uint256[] memory counts = new uint256[](ballots[_ballotAddress].options.length);
+        for (uint8 i = 0; i < ballots[_ballotAddress].options.length; i++) {
+            counts[i] = voteCounts[_ballotAddress][i];
+        }
+        return counts;
+    }
+
+    function vote(bytes32 _ballotAddress, string memory _passport, uint8 _option) ballotExists(_ballotAddress) external {
+        require(!hasEnded(_ballotAddress), "Voting has ended.");
+        require(_option < ballots[_ballotAddress].options.length, "Invalid option.");
+        require(!hasVoted[_ballotAddress][_passport], "User has already voted.");
+        hasVoted[_ballotAddress][_passport] = true;
+        voteCounts[_ballotAddress][_option]++;
+        emit VoteCreated(_ballotAddress, _passport, _option);
     }
 }
-
--- core/web/blockchain.py
+++ core/web/blockchain.py
@@ -1,77 +1,103 @@
 import json
-import web3
+import os
+
+from decouple import config
+from django.conf import settings
+from web3 import HTTPProvider, Web3
 from web3.exceptions import ContractLogicError
 
-BLOCKCHAIN_URL = "http://127.0.0.1"
-USERS_BLOCKCHAIN_PORT = 7545
-VOTES_BLOCKCHAIN_PORT = 7546
-
-USERS_PAYER_ADDRESS = ...
-VOTES_PAYER_ADDRESS = ...
-
-USERS_CONTRACT_ADDRESS = ...
-VOTES_CONTRACT_ADDRESS = ...
-
-USERS_ABI_PATH = "../../blockchain/build/contracts/Users.json"
-VOTES_ABI_PATH = "../../blockchain/voting/build/contracts/Voting.json"
-
-users_w3 = web3.Web3(web3.HTTPProvider(f"{BLOCKCHAIN_URL}:{USERS_BLOCKCHAIN_PORT}"))
-votes_w3 = web3.Web3(web3.HTTPProvider(f"{BLOCKCHAIN_URL}:{VOTES_BLOCKCHAIN_PORT}"))
-
-users_contract_address = users_w3.to_checksum_address(USERS_CONTRACT_ADDRESS)
-votes_contract_address = votes_w3.to_checksum_address(VOTES_CONTRACT_ADDRESS)
-
-with open(USERS_ABI_PATH) as json_file:
-    _json_obj = json.load(json_file)
-    USERS_ABI = _json_obj['abi']
-
-users_contract = users_w3.eth.contract(users_contract_address, abi=USERS_ABI)
-
-with open(VOTES_ABI_PATH) as json_file:
-    _json_obj = json.load(json_file)
-    VOTES_ABI = _json_obj['abi']
-
-votes_contract = votes_w3.eth.contract(votes_contract_address, abi=VOTES_ABI)
+BLOCKCHAIN_URL = config("BLOCKCHAIN_URL")
 
 
-def get_blockchain_user(passport_hash: str):
-    try:
-        user_id, user_data = users_contract.functions.getUser(passport_hash).call()
-        return user_id, user_data
-    except ValueError as e:
-        print(e.args[0]["message"])
+class BlockchainUserManager:
+
+    PORT = 7545
+    PATH = os.path.join(settings.BASE_DIR, "blockchain", "users", "build", "contracts", "Users.json")
+
+    def __init__(self):
+        self.web3 = Web3(HTTPProvider(f"{BLOCKCHAIN_URL}:{self.PORT}"))
+
+        self.payer = self.web3.eth.accounts[0]
+
+        with open(self.PATH) as file:
+            data = json.load(file)
+            self.contract = self.web3.eth.contract(
+                self.web3.to_checksum_address(data["networks"]["5777"]["address"]), abi=data["abi"]
+            )
+
+    def get_transaction(self, txHash):
+        return self.web3.eth.get_transaction(txHash)
+
+    def create_user(self, passport, full_name, birthday):
+        try:
+            txHash = self.contract.functions.createUser(passport, full_name, birthday).transact({"from": self.payer})
+            return txHash
+        except ContractLogicError:
+            return None
+
+    def get_user(self, passport):
+        try:
+            return self.contract.functions.getUser(passport).call()
+        except ValueError:
+            return None
 
 
-def create_blockchain_user(passport_hash: str, user_data: str):
-    try:
-        users_contract.functions.createUser(passport_hash, user_data).transact({"from": USERS_PAYER_ADDRESS})
-        return True
-    except ContractLogicError as e:
-        print(e)
+class BlockchainVotingManager:
 
+    PORT = 7546
+    PATH = os.path.join(settings.BASE_DIR, "blockchain", "voting", "build", "contracts", "Voting.json")
 
-def create_bill(title: str, options: list[str], duration: int):
-    try:
-        votes_contract.functions.createBill(title, options, duration).transact({"from": VOTES_PAYER_ADDRESS})
-        return True
-    except Exception as e:
-        print(e)
+    def __init__(self):
+        self.web3 = Web3(HTTPProvider(f"{BLOCKCHAIN_URL}:{self.PORT}"))
 
+        self.payer = self.web3.eth.accounts[0]
 
-def get_bill_options(bill_id: int):
-    try:
-        options = votes_contract.functions.getBillOptions(bill_id).call()
-        return options
-    except ValueError as e:
-        print(e.args[0]["message"])
+        with open(self.PATH) as file:
+            data = json.load(file)
+            self.contract = self.web3.eth.contract(
+                self.web3.to_checksum_address(data["networks"]["5777"]["address"]), abi=data["abi"]
+            )
 
+    def get_transaction(self, txHash):
+        return self.web3.eth.get_transaction(txHash)
 
-def vote(bill_id: int, option_index: int, passport_hash: str):
-    if not get_blockchain_user(passport_hash):
-        return None
-    try:
-        votes_contract.functions.vote(bill_id, option_index, passport_hash).transact({"from": VOTES_PAYER_ADDRESS})
-    except ContractLogicError as e:
-        print(e)
-    except ValueError as e:
-        print(e)
+    def create_ballot(self, title, options, duration):
+        try:
+            tx_hash = self.contract.functions.createBallot(title, options, duration).transact({"from": self.payer})
+            tx_receipt = self.web3.eth.wait_for_transaction_receipt(tx_hash)
+            events = self.contract.events.BallotCreated().process_receipt(tx_receipt)
+            address = events[0].args.ballotAddress
+            return tx_hash, address
+        except ContractLogicError:
+            return None, None
+
+    def get_ballot(self, address):
+        try:
+            return self.contract.functions.getBallot(address).call()
+        except ValueError:
+            return None
+
+    def has_ended(self, address):
+        try:
+            return self.contract.functions.hasEnded(address).call()
+        except ValueError:
+            return None
+
+    def has_vote(self, address, passport):
+        try:
+            return self.contract.functions.hasVote(address, passport).call()
+        except ValueError:
+            return None
+
+    def get_vote_counts(self, address):
+        try:
+            return self.contract.functions.getVoteCounts(address).call()
+        except ValueError:
+            return None
+
+    def vote(self, ballot_address, passport, option):
+        try:
+            txHash = self.contract.functions.vote(ballot_address, passport, option).transact({"from": self.payer})
+            return txHash
+        except (ContractLogicError, ValueError):
+            return None
