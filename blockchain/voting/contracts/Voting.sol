d23b8dcc1d60cfc0f023466b228a1e4ceaf1be4e
MagisterFelix <felix.vlad.14@gmail.com>
2023-03-19 00:41:37 +0200

    Fix contracts

--- blockchain/voting/contracts/Voting.sol
+++ blockchain/voting/contracts/Voting.sol
@@ -6,54 +6,51 @@ contract Voting {
     struct Ballot {
         string title;
         string[] options;
-        uint256 endTime;
+        uint256 endDate;
     }
 
     mapping(bytes32 => Ballot) private ballots;
-    mapping(bytes32 => mapping(string => bool)) private hasVoted;
-    mapping(bytes32 => mapping(uint8 => uint256)) private voteCounts;
+    mapping(bytes32 => mapping(string => uint8)) private votes;
+    mapping(bytes32 => mapping(uint8 => uint256)) private countOfVotes;
 
-    modifier ballotExists(bytes32 _ballotAddress) {
-        require(bytes(ballots[_ballotAddress].title).length > 0, "Ballot not found.");
+    modifier ballotExists(bytes32 _ballotId) {
+        require(bytes(ballots[_ballotId].title).length > 0, "Ballot not found.");
         _;
     }
 
-    event BallotCreated(bytes32 ballotAddress, string title, string[] options, uint256 endTime);
-    event VoteCreated(bytes32 ballotAddress, string passport, uint8 option);
+    event BallotCreated(bytes32 id, string title, string[] options, uint256 endDate);
+    event VoteCreated(bytes32 ballot_id, string passport, uint8 option);
 
-    function createBallot(string memory _title, string[] memory _options, uint256 _duration) external {
+    function createBallot(string memory _title, string[] memory _options, uint256 _endDate) external {
+        require(block.timestamp < _endDate, "Invalid date.");
         bytes32 id = keccak256(abi.encodePacked(_title, block.timestamp));
-        uint256 endTime = block.timestamp + _duration;
-        ballots[id] = Ballot(_title, _options, endTime);
-        emit BallotCreated(id, _title, _options, endTime);
+        ballots[id] = Ballot(_title, _options, _endDate);
+        emit BallotCreated(id, _title, _options, _endDate);
     }
 
-    function getBallot(bytes32 _ballotAddress) ballotExists(_ballotAddress) external view returns (Ballot memory) {
-        return ballots[_ballotAddress];
-    }
-
-    function hasEnded(bytes32 _ballotAddress) ballotExists(_ballotAddress) public view returns (bool) {
-        return ballots[_ballotAddress].endTime < block.timestamp;
-    }
-
-    function hasVote(bytes32 _ballotAddress, string memory _passport) ballotExists(_ballotAddress) external view returns (bool) {
-        return hasVoted[_ballotAddress][_passport];
-    }
-
-    function getVoteCounts(bytes32 _ballotAddress) ballotExists(_ballotAddress) external view returns (uint256[] memory) {
-        uint256[] memory counts = new uint256[](ballots[_ballotAddress].options.length);
-        for (uint8 i = 0; i < ballots[_ballotAddress].options.length; i++) {
-            counts[i] = voteCounts[_ballotAddress][i];
+    function getBallot(bytes32 _ballotId) ballotExists(_ballotId) external view returns (string memory, string[] memory, uint256, uint256[] memory) {
+        uint256[] memory countOfVotesByOption = new uint256[](ballots[_ballotId].options.length);
+        for (uint8 i = 0; i < ballots[_ballotId].options.length; i++) {
+            countOfVotesByOption[i] = countOfVotes[_ballotId][i + 1];
         }
-        return counts;
+        return (ballots[_ballotId].title, ballots[_ballotId].options, ballots[_ballotId].endDate, countOfVotesByOption);
     }
 
-    function vote(bytes32 _ballotAddress, string memory _passport, uint8 _option) ballotExists(_ballotAddress) external {
-        require(!hasEnded(_ballotAddress), "Voting has ended.");
-        require(_option < ballots[_ballotAddress].options.length, "Invalid option.");
-        require(!hasVoted[_ballotAddress][_passport], "User has already voted.");
-        hasVoted[_ballotAddress][_passport] = true;
-        voteCounts[_ballotAddress][_option]++;
-        emit VoteCreated(_ballotAddress, _passport, _option);
+    function hasVote(bytes32 _ballotId, string memory _passport) ballotExists(_ballotId) public view returns (bool) {
+        return votes[_ballotId][_passport] != 0;
+    }
+
+    function getVote(bytes32 _ballotId, string memory _passport) ballotExists(_ballotId) external view returns (uint8) {
+        require(hasVote(_ballotId, _passport), "User has not voted.");
+        return votes[_ballotId][_passport];
+    }
+
+    function vote(bytes32 _ballotId, string memory _passport, uint8 _option) ballotExists(_ballotId) external {
+        require(block.timestamp < ballots[_ballotId].endDate, "Voting has ended.");
+        require(_option != 0 && _option <= ballots[_ballotId].options.length, "Invalid option.");
+        require(!hasVote(_ballotId, _passport), "User has already voted.");
+        votes[_ballotId][_passport] = _option;
+        countOfVotes[_ballotId][_option]++;
+        emit VoteCreated(_ballotId, _passport, _option);
     }
 }
